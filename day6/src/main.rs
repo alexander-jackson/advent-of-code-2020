8bfe36e30740eff5107ecc80252bf3c6cc4d0594
Alexander Jackson <alexanderjackson@protonmail.com>
2020-12-06 08:05:33 +0000

    Add solution for Part 2 of Day 6

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,11 +1,11 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn read_input() -> Vec<String> {
     std::fs::read_to_string("input.txt")
         .unwrap()
         .trim()
         .split("\n\n")
-        .map(|x| x.replace("\n", ""))
+        .map(|x| x.into())
         .collect()
 }
 
@@ -14,7 +14,18 @@ fn main() {
 
     let count: usize = input
         .iter()
-        .map(|x| x.chars().collect::<HashSet<_>>().len())
+        .map(|x| {
+            let mut map = HashMap::new();
+
+            for c in x.chars() {
+                let entry = map.entry(c).or_insert(0);
+                *entry += 1;
+            }
+
+            let people = map.get(&'\n').map(|x| *x).unwrap_or_default() + 1;
+
+            map.iter().filter(|(_, v)| **v == people).count()
+        })
         .sum();
 
     dbg!(&input);
