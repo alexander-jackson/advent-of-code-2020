406356929349bfcb115815ff9230e2186cb9c1a4
Alexander Jackson <alexanderjackson@protonmail.com>
2020-12-10 11:40:29 +0000

    Add solution for Part 2 of Day 10

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,16 +1,42 @@
+use std::collections::HashMap;
 use std::str::FromStr;
+
+fn count_combinations(adapters: &[i64]) -> i64 {
+    let mut counts: HashMap<i64, i64> = HashMap::new();
+
+    // Start with our initial value
+    counts.insert(0, 1);
+
+    let end = *adapters.last().unwrap();
+
+    // Iterate dynamically
+    for i in 1..=end {
+        if !adapters.contains(&i) {
+            continue;
+        }
+
+        // Find the routes that we could have come from
+        let routes: i64 = (1..=3)
+            .filter(|j| counts.contains_key(&(i - j)))
+            .map(|j| counts[&(i - j)])
+            .sum();
+
+        counts.insert(i, routes);
+    }
+
+    dbg!(counts[&end])
+}
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
     let mut formatted: Vec<_> = input
         .trim()
         .lines()
-        .map(|l| u32::from_str(l).unwrap())
+        .map(|l| i64::from_str(l).unwrap())
         .collect();
 
     formatted.sort();
-
-    let adapter = formatted.last().unwrap() + 3;
+    dbg!(&formatted);
 
     let one_gaps = formatted
         .windows(2)
@@ -35,4 +61,7 @@ fn main() {
     dbg!(&three_gaps);
 
     println!("Solution: {}", one_gaps * three_gaps);
+
+    let combinations = count_combinations(&formatted);
+    dbg!(&combinations);
 }
