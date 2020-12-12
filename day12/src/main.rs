97c4a38e706a529c8758961eea300e49e79703ff
Alexander Jackson <alexanderjackson@protonmail.com>
2020-12-12 07:55:00 +0000

    Add solution for Part 2 of Day 12

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,5 +1,11 @@
-use std::ops::AddAssign;
+use std::ops::{AddAssign, Mul};
 use std::str::FromStr;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum Mode {
+    Normal,
+    Waypoint,
+}
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 enum Heading {
@@ -45,6 +51,39 @@ struct Coordinate {
     pub y: i32,
 }
 
+impl Coordinate {
+    pub fn update(&mut self, modifier: char, value: i32) {
+        match modifier {
+            'N' => self.y += value,
+            'E' => self.x += value,
+            'S' => self.y -= value,
+            'W' => self.x -= value,
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn rotate(&mut self, degrees: i32) {
+        let s = (-degrees as f32).to_radians().sin() as i32;
+        let c = (-degrees as f32).to_radians().cos() as i32;
+
+        *self = Self {
+            x: self.x * c - self.y * s,
+            y: self.x * s + self.y * c,
+        }
+    }
+}
+
+impl Mul<i32> for Coordinate {
+    type Output = Self;
+
+    fn mul(self, value: i32) -> Self {
+        Self {
+            x: self.x * value,
+            y: self.y * value,
+        }
+    }
+}
+
 impl AddAssign for Coordinate {
     fn add_assign(&mut self, other: Self) {
         *self = Self {
@@ -58,6 +97,7 @@ impl AddAssign for Coordinate {
 struct Orienteer {
     heading: Heading,
     position: Coordinate,
+    waypoint: Coordinate,
 }
 
 impl Orienteer {
@@ -65,18 +105,38 @@ impl Orienteer {
         Default::default()
     }
 
-    pub fn simulate(&mut self, moves: Vec<(char, i32)>) -> Coordinate {
+    pub fn simulate(&mut self, moves: &[(char, i32)], mode: Mode) -> Coordinate {
+        match mode {
+            Mode::Normal => self.simulate_normal(moves),
+            Mode::Waypoint => self.simulate_waypoint(moves),
+        }
+    }
+
+    fn simulate_normal(&mut self, moves: &[(char, i32)]) -> Coordinate {
         for instruction in moves {
-            let (modifier, value) = instruction;
+            let (modifier, value) = *instruction;
 
             match modifier {
-                'N' => self.position.y += value,
-                'E' => self.position.x += value,
-                'S' => self.position.y -= value,
-                'W' => self.position.x -= value,
+                'N' | 'E' | 'S' | 'W' => self.position.update(modifier, value),
                 'L' => self.heading.rotate(360 - value),
                 'R' => self.heading.rotate(value),
                 'F' => self.position += self.heading.modifier(value),
+                _ => unreachable!(),
+            }
+        }
+
+        self.position
+    }
+
+    fn simulate_waypoint(&mut self, moves: &[(char, i32)]) -> Coordinate {
+        for instruction in moves {
+            let (modifier, value) = *instruction;
+
+            match modifier {
+                'N' | 'E' | 'S' | 'W' => self.waypoint.update(modifier, value),
+                'L' => self.waypoint.rotate(360 - value),
+                'R' => self.waypoint.rotate(value),
+                'F' => self.position += self.waypoint * value,
                 _ => unreachable!(),
             }
         }
@@ -90,6 +150,7 @@ impl Default for Orienteer {
         Self {
             heading: Heading::East,
             position: Default::default(),
+            waypoint: Coordinate { x: 10, y: 1 },
         }
     }
 }
@@ -109,7 +170,12 @@ fn main() {
         .collect();
 
     let mut orienteer = Orienteer::new();
-    let position = orienteer.simulate(moves);
+    let position = orienteer.simulate(&moves, Mode::Normal);
 
     println!("Part 1 Solution: {}", position.x.abs() + position.y.abs());
+
+    let mut orienteer = Orienteer::new();
+    let position = orienteer.simulate(&moves, Mode::Waypoint);
+
+    println!("Part 2 Solution: {}", position.x.abs() + position.y.abs());
 }
